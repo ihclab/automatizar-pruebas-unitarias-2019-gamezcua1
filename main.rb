b29925cbde043bad5cebd61f08fa162825efeae0
Gerald Amezcua <gamezcua1@ucol.mx>
2019-03-19 09:48:45 -0600

    Problema 02

--- main.rb
+++ main.rb
@@ -1,6 +1,14 @@
 require_relative "Medias"
 
-p Medias.mediaAritmetica([1, 2])
-p Medias.mediaGeometrica([2, 18])
-p Medias.mediaArmonica([1, 2, 3])
-p Medias.raizEnesima(1, 2)
+require_relative "Medias"
+
+pruebas_file_path = ARGV[0]
+
+if pruebas_file_path == nil
+  puts "No introdujo el nombre del archivo de pruebas"
+  return 0
+end
+
+File.readlines(pruebas_file_path).each do |line|
+  print line
+end
