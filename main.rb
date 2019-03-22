71118cc67d603e81db0b644f131cf6e6498e3a28
Gerald Amezcua <gamezcua1@ucol.mx>
2019-03-21 19:52:09 -0600

    Problema 08

--- main.rb
+++ main.rb
@@ -5,6 +5,8 @@ require_relative "Helpers"
 require_relative "Asserts"
 
 pruebas_file_path = ARGV[0]
+success = 0
+failed = 0
 
 if pruebas_file_path == nil
   puts "No introdujo el nombre del archivo de pruebas"
@@ -12,6 +14,9 @@ if pruebas_file_path == nil
 end
 
 begin
+  puts "ID\tResultado\tMétodo\t\t\tDetalles"
+  puts "=========================================================="
+
   File.readlines(pruebas_file_path).each do |line|
     test_case = Helpers.get_test_params line    # Caso de prueba
     values = Helpers.get_values test_case[2]    # Parámetros que nuestras funciones recibirán
@@ -21,13 +26,21 @@ begin
       res = Medias.send(test_case[1], values)
       assert = Asserts.assertEqual res, waited_reult
 
-      p assert ? "Éxito" : "*Falla*"
+      if assert
+        puts "#{test_case[0]}\t#{ "Éxito".green }\t\t#{test_case[1]}\t\tCalculado = #{res}"
+        success += 1
+      else
+        puts "#{test_case[0]}\t#{ "Falla".red }\t\t#{test_case[1]}\t\tCalculado = #{res} \t Esperado = #{test_case[3]}"
+        failed += 1
+      end
     rescue NoMethodError
-      p "Método no existe"
+      puts "#{test_case[0]}\t\t\t #{test_case[1]} no existe"
     rescue NotImplementedError
-      p "Método no implementado"
+      puts "#{test_case[0]}\t\t\t #{test_case[1]} no implementado"
     end
   end
+
+  puts "\t\t#{ "Éxito = ".green }#{ success.to_s.green }\t\t#{ "Falla = ".red }#{ failed.to_s.red }"
 
 rescue Errno::ENOENT
   p "El archivo introducido no existe"
